8d58e041e9a9144a278c0b2333cd0d3af514bb9d
ILSUR26 <86534600+GalievIlsur@users.noreply.github.com>
2021-06-27 23:46:51 +0300

    MySpace
    MySpace

--- CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/util/Util.java
+++ CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static void getConnect() {
+        try{
+            String url = "jdbc:mysql://localhost:3306/usersdb";
+            String username = "ILSUR26";
+            String password = "F7atx538ilsur";
+            try (Connection conn = DriverManager.getConnection(url, username, password)){
+                System.out.println("Connection to DB success!");
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+
+            System.out.println(ex);
+        }
+    }
+
 }
