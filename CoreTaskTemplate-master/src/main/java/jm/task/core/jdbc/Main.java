cbf6355a9a5655d9a75721ed6b6946dc35906c46
ILSUR26 <86534600+GalievIlsur@users.noreply.github.com>
2021-06-29 11:31:29 +0300

    MySpace
    MySpace

--- CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/Main.java
+++ CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -12,20 +12,20 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws SQLException {
         UserDao userDao = new UserDaoJDBCImpl();
-       userDao.createUsersTable();
+        userDao.createUsersTable();
 
-//        userDao.saveUser("Alex", "Niko", (byte)37);
-//        userDao.saveUser("John", "Kramer", (byte)55);
-//        userDao.saveUser("Alice", "Karter", (byte)34);
-//        userDao.saveUser("Bin", "Mister", (byte)60);
-//
-//        List<User> userList = userDao.getAllUsers();
-//        for(User u : userList) {
-//            System.out.println(u.toString());
-//        }
-//        userDao.removeUserById(1);
-//        userDao.cleanUsersTable();
-//        userDao.dropUsersTable();
+        userDao.saveUser("Alex", "Niko", (byte)37);
+        userDao.saveUser("John", "Kramer", (byte)55);
+        userDao.saveUser("Alice", "Karter", (byte)34);
+        userDao.saveUser("Bin", "Mister", (byte)60);
+
+        List<User> userList = userDao.getAllUsers();
+        for(User u : userList) {
+            System.out.println(u.toString());
+        }
+
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
 
 
     }
