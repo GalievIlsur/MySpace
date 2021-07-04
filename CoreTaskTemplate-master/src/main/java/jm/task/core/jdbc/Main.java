74433b33025d3ed622a590248002f707da3422e8
ILSUR26 <86534600+GalievIlsur@users.noreply.github.com>
2021-07-04 18:18:52 +0300

    UserDaoJDBCImpl
    Финальная версия работы через UserDaoJDBCImpl (Исправил класс Main)

--- CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/Main.java
+++ CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -2,13 +2,14 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import java.sql.*;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserDao userDao = new UserDaoHibernateImpl();
+        UserDao userDao = new UserDaoJDBCImpl();
         userDao.createUsersTable();
 
         userDao.saveUser("Alex", "Niko", (byte)37);
@@ -16,8 +17,8 @@ public class Main {
         userDao.saveUser("Alice", "Karter", (byte)34);
         userDao.saveUser("Bin", "Mister", (byte)60);
 
-        List<User> userList2 = userDao.getAllUsers();
-        for(User u : userList2) {
+        List<User> userList = userDao.getAllUsers();
+        for(User u : userList) {
             System.out.println(u.toString());
         }
 
