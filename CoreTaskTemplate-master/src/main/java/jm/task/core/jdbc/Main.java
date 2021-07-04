e7ec5e3ab165bbde7b701eb0b6dba6b6043b780e
ILSUR26 <86534600+GalievIlsur@users.noreply.github.com>
2021-07-04 17:52:19 +0300

    UserDaoHibernateImpl
    Работа через UserDaoHibernateImpl

--- CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/Main.java
+++ CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -3,17 +3,14 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
-import java.lang.reflect.InvocationTargetException;
 import java.sql.*;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoHibernateImpl();
         userDao.createUsersTable();
 
         userDao.saveUser("Alex", "Niko", (byte)37);
@@ -21,18 +18,12 @@ public class Main {
         userDao.saveUser("Alice", "Karter", (byte)34);
         userDao.saveUser("Bin", "Mister", (byte)60);
 
-        List<User> userList = userDao.getAllUsers();
-        for(User u : userList) {
+        List<User> userList2 = userDao.getAllUsers();
+        for(User u : userList2) {
             System.out.println(u.toString());
         }
 
         userDao.cleanUsersTable();
         userDao.dropUsersTable();
-
-
-
-
-
-
     }
 }
--- CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,82 +2,108 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    public static Transaction transaction = null;
 
     public void createUsersTable() {
-        try(Statement statement = Util.getStatement()) {
-            String sql = new StringBuilder().append("create table if not exists myUsers")
-                    .append("(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,")
-                    .append("name VARCHAR(45),")
-                    .append("lastName VARCHAR(45),")
-                    .append("age INTEGER(3))")
-                    .toString();
+        String sql = new StringBuilder().append("create table if not exists myusers")
+                .append("(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,")
+                .append("name VARCHAR(45),")
+                .append("lastName VARCHAR(45),")
+                .append("age INTEGER(3))")
+                .toString();
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createNativeQuery(sql).executeUpdate();
+            session.getTransaction().commit();
 
-            statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            System.out.println(e);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+                e.printStackTrace();
+            }
         }
     }
 
     public void dropUsersTable() {
-        try(Statement statement = Util.getStatement()) {
-            statement.execute("drop table if exists myusers");
-        } catch (SQLException ex) {
-            System.out.println(ex);
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createNativeQuery("drop table if exists myusers").executeUpdate();
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+                e.printStackTrace();
+            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try(Statement statement = Util.getStatement()) {
-            String sql = new StringBuilder().append("insert into myusers (name, lastName, age) values ('")
-                    .append(name).append("', '")
-                    .append(lastName).append("', ")
-                    .append(age).append(")")
-                    .toString();
-            statement.execute(sql);
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            session.getTransaction().commit();
             System.out.println("User с именем " + name + " добавлен в базу данных");
-        } catch (SQLException e) {
-            System.out.println(e);
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+                e.printStackTrace();
+            }
         }
     }
 
     public void removeUserById(long id) {
-        try(Statement statement = Util.getStatement()) {
-            statement.execute("delete from myusers where id = " + id + ";");
-        } catch (SQLException ex) {
-            System.out.println(ex);
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createNativeQuery("delete from myusers where id = " + id + ";").executeUpdate();
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+                e.printStackTrace();
+            }
         }
     }
 
     public List<User> getAllUsers() {
-        List<User> result = new ArrayList<>();
-        try (ResultSet res = Util.getStatement().executeQuery("select * from myusers")) {
-            while (res.next()) {
-                User user = new User();
-                user.setId(res.getLong("id"));
-                user.setName(res.getString("name"));
-                user.setLastName(res.getString("lastName"));
-                user.setAge(res.getByte("age"));
-                result.add(user);
+        List<User> newResult = new ArrayList<>();
+
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Query<User> query = session.createQuery("from User");
+            newResult = query.getResultList();
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+                e.printStackTrace();
             }
-        } catch (SQLException ex) {
-            System.out.println(ex);
         }
-        return result;
+        return newResult;
     }
 
     public void cleanUsersTable() {
-        try(Statement statement = Util.getStatement()) {
-            statement.execute("truncate table myusers");
-        } catch (SQLException ex) {
-            System.out.println(ex);
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createNativeQuery("truncate table myusers").executeUpdate();
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+                e.printStackTrace();
+            }
         }
     }
 }
